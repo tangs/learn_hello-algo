964016056b5127c17b633ea47d727157342625e6
tangs <tangsong1988kaka@gmail.com>
2023-12-20 18:15:06 +0800

    add minPathSumDP solution.

--- chapter_dynamic_programming/dp_solution_pipeline/min_path_sum.cpp
+++ chapter_dynamic_programming/dp_solution_pipeline/min_path_sum.cpp
@@ -28,6 +28,20 @@ int minPathSumDFSMen(std::vector<std::vector<int>> &grid, std::vector<std::vecto
   return ret;
 }
 
+int minPathSumDP(std::vector<std::vector<int>> grid) {
+  auto row = grid.size();
+  auto col = grid[0].size();
+  std::vector<std::vector<int>> dp(row + 1, std::vector<int>(col + 1, 0));
+
+  for (int i = 1; i <= row; ++i) {
+    for (int j = 1; j <= col; ++j) {
+      dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+    }
+  }
+
+  return dp[row][col];
+}
+
 int main() {
 //  std::vector<std::vector<int>> grid0 = {
 //          {1, 3, 1, 5},
@@ -36,7 +50,7 @@ int main() {
 //          {4, 3, 5, 2},
 //  };
   constexpr auto ROW = 14;
-  constexpr auto COL = 18;
+  constexpr auto COL = 20;
   std::vector<std::vector<int>> grid0;
   for (int i = 0; i < ROW; ++i) {
     grid0.emplace_back();
@@ -51,12 +65,16 @@ int main() {
   const auto time1{std::chrono::steady_clock::now()};
   auto ret1 = minPathSumDFSMen(grid0, men, (int)grid0.size() - 1, (int)grid0[0].size() - 1);
   const auto time2{std::chrono::steady_clock::now()};
+  auto ret2 = minPathSumDP(grid0);
+  const auto time3{std::chrono::steady_clock::now()};
 
   const std::chrono::duration<double> time_seconds0{time1 - time0};
   const std::chrono::duration<double> time_seconds1{time2 - time1};
+  const std::chrono::duration<double> time_seconds2{time3 - time2};
 
   std::cout << ret0 << ", " << time_seconds0.count() << std::endl;
   std::cout << ret1 << ", " << time_seconds1.count() << std::endl;
+  std::cout << ret2 << ", " << time_seconds2.count() << std::endl;
 
   return 0;
 }
