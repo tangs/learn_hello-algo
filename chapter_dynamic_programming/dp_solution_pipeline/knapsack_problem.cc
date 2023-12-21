765b99b6dff13f042e6dc67f12690e7736a04b61
tangs <tangsong1988kaka@gmail.com>
2023-12-21 16:39:46 +0800

    add knapsack dfs men solution.

--- chapter_dynamic_programming/dp_solution_pipeline/knapsack_problem.cc
+++ chapter_dynamic_programming/dp_solution_pipeline/knapsack_problem.cc
@@ -11,10 +11,28 @@ int knapsackDFS(std::vector<int> &wgt, std::vector<int> &val, int i, int c) {
   return std::max(no, yes);
 }
 
+int knapsackDFSMen(std::vector<int> &wgt, std::vector<int> &val, std::vector<std::vector<int>> &men, int i, int c) {
+  if (auto ret = men[i][c]; ret != -1) return ret;
+  if (i == 0) return c >= wgt[0] ? val[0] : 0;
+  if (c < wgt[i]) return knapsackDFSMen(wgt, val, men, i - 1, c);
+
+  auto no = knapsackDFSMen(wgt, val, men, i - 1, c);
+  auto yes = knapsackDFSMen(wgt, val, men, i - 1, c - wgt[i]) + val[i];
+
+  auto ret = std::max(no, yes);
+  men[i][c] = ret;
+  return ret;
+}
+
 int main() {
   std::vector<int> wgt = {10, 20, 30, 40, 50};
   std::vector<int> val = {50, 120, 150, 210, 240};
+  const auto knapsackCapacity = 50;
 
-  auto ret0 = knapsackDFS(wgt, val, wgt.size() - 1, 50);
+  std::vector<std::vector<int>> men(wgt.size(), std::vector<int>(knapsackCapacity + 1, -1));
+
+  auto ret0 = knapsackDFS(wgt, val, wgt.size() - 1, knapsackCapacity);
+  auto ret1 = knapsackDFSMen(wgt, val, men, wgt.size() - 1, knapsackCapacity);
+
   return 0;
 }
